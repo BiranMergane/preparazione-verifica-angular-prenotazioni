b68c47eaa0f23c8c4e3ac7381951e94f7f95d241
Biran Mergane <b.mergane2007@gmail.com>
2025-05-16 08:52:48 +0000

    Settimo Commit

--- PreparazioneVerifica/src/app/app.component.ts
+++ PreparazioneVerifica/src/app/app.component.ts
@@ -22,14 +22,46 @@ export class AppComponent implements OnInit {
   loading!: boolean
   o_vettPrenotazioni!: Observable<Prenotazioni[]>
   http!: HttpClient
+  o!: Observable <Object>
+  dati_post: Object = JSON.stringify({})
+  data!: Object
 
   constructor(http: HttpClient){this.http = http}
 
 
 salva(nome: HTMLInputElement, cognome: HTMLInputElement, indirizzo: HTMLInputElement, email: HTMLInputElement, telefono: HTMLInputElement, data: HTMLInputElement, ora: HTMLInputElement){
-  this.vettPrenotazioni.push(new Prenotazioni(nome.value, cognome.value, indirizzo.value, telefono.value, email.value, data.value, ora.value ))
-  console.log(this.vettPrenotazioni)
+  this.loading = true
+    this.dati_post = JSON.stringify(
+      {
+        nome: nome.value,
+        cognome: cognome.value,
+        indirizzo: indirizzo.value,
+        telefono: telefono.value,
+        email: email.value,
+        data: data.value,
+        ora: ora.value
+      }
+    )
+    this.o = this.http.post("https://my-json-server.typicode.com/malizia-g/verificaPrenotazioni/prenotazioni", this.dati_post)
+    this.o.subscribe(this.postData)
+    this.vettPrenotazioni.push(new Prenotazioni(nome.value, cognome.value, indirizzo.value, telefono.value, email.value, data.value, ora.value))
+    console.log(this.vettPrenotazioni)
+    nome.value = ""
+    cognome.value = ""
+    indirizzo.value = ""
+    telefono.value = ""
+    email.value = ""
+    data.value = ""
+    ora.value = ""
 }
+
+postData = (d: object) =>
+  {
+    this.data = d
+    console.log(this.data)
+    this.loading = false
+  }
+
 makeGet()
 {
   this.loading = true
